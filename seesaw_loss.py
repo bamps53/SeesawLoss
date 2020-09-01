afc670aa4379853a3fe100a5f0bbee29447663d9
bamps53 <37071446+bamps53@users.noreply.github.com>
2020-09-01 19:56:18 +0900

    Update seesaw_loss.py

--- seesaw_loss.py
+++ seesaw_loss.py
@@ -14,7 +14,7 @@ class SeesawLossWithLogits(nn.Module):
     class_counts: The list which has number of samples for each class. 
                   Should have same length as num_classes.
     p: Scale parameter which adjust the strength of panishment.
-       Set to 0.8 for default following the paper.
+       Set to 0.8 as a default by following the original paper.
     """
 
     def __init__(self, class_counts: Union[list, np.array], p: float = 0.8):
@@ -22,21 +22,24 @@ class SeesawLossWithLogits(nn.Module):
 
         class_counts = torch.FloatTensor(class_counts)
         conditions = class_counts[:, None] > class_counts[None, :]
-        trues = (class_counts[None, :] / class_counts[:, None])**p
+        trues = (class_counts[None, :] / class_counts[:, None]) ** p
         falses = torch.ones(len(class_counts), len(class_counts))
         self.s = torch.where(conditions, trues, falses)
+        
+        self.eps = 1.0e-6
 
     def forward(self, logits, targets):
+        self.s = self.s.to(targets.device)
         max_element, _ = logits.max(axis=-1)
         logits = logits - max_element[:, None]  # to prevent overflow
 
         numerator = torch.exp(logits)
         denominator = (
             (1 - targets)[:, None, :]
-            * s[None, :, :]
+            * self.s[None, :, :]
             * torch.exp(logits)[:, None, :]).sum(axis=-1) \
             + torch.exp(logits)
 
-        sigma = numerator / (denominator + 1.0e-6)
-        loss = (- targets * torch.log(sigma)).sum(-1)
+        sigma = numerator / (denominator + self.eps)
+        loss = (- targets * torch.log(sigma + self.eps)).sum(-1)
         return loss.mean()
