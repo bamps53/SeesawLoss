476d60f6c8528f26491a34e397396b2b06ed380d
bamps53 <37071446+bamps53@users.noreply.github.com>
2020-09-02 12:03:46 +0900

    add DistibutionAgnosticSeesawLossWithLogits

--- seesaw_loss.py
+++ seesaw_loss.py
@@ -43,3 +43,47 @@ class SeesawLossWithLogits(nn.Module):
         sigma = numerator / (denominator + self.eps)
         loss = (- targets * torch.log(sigma + self.eps)).sum(-1)
         return loss.mean()
+
+    
+class DistibutionAgnosticSeesawLossWithLogits(nn.Module):
+    """
+    This is unofficial implementation for Seesaw loss,
+    which is proposed in the techinical report for LVIS workshop at ECCV 2020.
+    For more detail, please refer https://arxiv.org/pdf/2008.10032.pdf.
+
+    Args:
+    p: Scale parameter which adjust the strength of panishment.
+       Set to 0.8 for default following the paper.
+    """
+
+    def __init__(self, p: float = 0.8):
+        super().__init__()
+        self.eps = 1.0e-6
+        self.p = p
+        self.s = None
+        self.class_counts = None
+
+    def forward(self, logits, targets):
+        if self.class_counts is None:
+            self.class_counts = targets.sum(axis=0) + 1 # to prevent devided by zero.
+        else:
+            self.class_counts += targets.sum(axis=0)
+
+        conditions = self.class_counts[:, None] > self.class_counts[None, :]
+        trues = (self.class_counts[None, :] / self.class_counts[:, None]) ** self.p
+        falses = torch.ones(len(self.class_counts), len(self.class_counts))
+        self.s = torch.where(conditions, trues, falses)
+
+        max_element, _ = logits.max(axis=-1)
+        logits = logits - max_element[:, None]  # to prevent overflow
+
+        numerator = torch.exp(logits)
+        denominator = (
+            (1 - targets)[:, None, :]
+            * self.s[None, :, :]
+            * torch.exp(logits)[:, None, :]).sum(axis=-1) \
+            + torch.exp(logits)
+
+        sigma = numerator / (denominator + self.eps)
+        loss = (- targets * torch.log(sigma + self.eps)).sum(-1)
+        return loss.mean()
